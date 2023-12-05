d6b6a604399df6050bccc23b12c877a51a8d7e1a
seungbeom4382 <jsd940@naver.com>
2023-12-05 10:08:03 +0900

    #231205_tailwindcss_복습수정

--- 06_tailwindcss/js/tailwind.config.js
+++ 06_tailwindcss/js/tailwind.config.js
@@ -3,6 +3,9 @@ tailwind.config = {
       extend: {
         colors: {
           clifford: '#da373d',
+        },
+        fontFamily : {
+          'sans' : ['Dongle', 'sans-serif'] // 기본 설정
         }
       }
     }
